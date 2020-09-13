7b3fe8e00e5b15db4052df88f6eba3d5f0b9e410
巴士 <tr-bus@outlook.com>
2020-09-13 22:26:05 +0800

    header 组件

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" @touchmove.prevent>
+    <v-header></v-header>
+    <div class="tab-wrapper">
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import VHeader from 'components/v-header/v-header'
+  import { getSeller } from './api'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    data () {
+      return {
+        seller: {}
+      }
+    },
+    created () {
+      getSeller().then((seller) => {
+        this.seller = seller
+      })
+    },
+    components: {
+      VHeader
+    }
   }
-}
 </script>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+<style lang="stylus" scoped>
+  #app
+    .tab-wrapper
+      position: fixed
+      top: 136px
+      left: 0
+      right: 0
+      bottom: 0
 </style>
